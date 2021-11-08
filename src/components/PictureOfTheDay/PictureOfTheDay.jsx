8c515a540f5db0db74d2bfa64210226694ad01da
vitaliiptp <vitalii.potapenko@gmail.com>
2021-11-08 11:29:18 +0100

    fixed style for iframe on pic of the day

--- src/components/PictureOfTheDay/PictureOfTheDay.jsx
+++ src/components/PictureOfTheDay/PictureOfTheDay.jsx
@@ -33,11 +33,13 @@ export default function NasaPhoto() {
 
                 <div className="flex-container">
                     <div className="flex-container--pic-of-day">
-                        {photoData.media_type === "image" ? (
-                            <img src={photoData.url} alt={photoData.title} className="photo" />
-                        ) : (
-                            <iframe title="space-video" src={photoData.url} className="photo" />
-                        )}
+                        <div className="flex-container--pic-of-day_img">
+                            {photoData.media_type === "image" ? (
+                                <img src={photoData.url} alt={photoData.title} className="photo" />
+                            ) : (
+                                <iframe title="space-video" src={photoData.url} className="photo" style={{width:"100%", height:"20rem"}}/>
+                            )}
+                        </div>
                         <div className="flex-container--pic-of-day_text">
                             <h1 className="text-accent fs-600 ff-sans-cond uppercase letter-spacing-1">{photoData.title}</h1>
                             <p className="text-accent fs-400 ff-sans-cond uppercase letter-spacing-3">{photoData.date}</p>
