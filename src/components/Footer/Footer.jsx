e7c39b1098e0b775dcfa3b74ba082a502278828e
vitaliiptp <vitalii.potapenko@gmail.com>
2021-11-11 13:45:20 +0100

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import { FaGithub } from 'react-icons/fa';
+import './styles.scss';
 
 
 
 const Footer = () => {
     return (
-        <div className="primary-navigation flex" style={{color: "white", justifyContent: "center"}}>
-            <ul style={{display: "flex"}}>
-                <li>WelcomeToSpace&copy;{new Date().getFullYear()}</li>
-                <li>
-                    <Link to="/contact">Contact Us</Link>
-                </li>
-                <li>GitHub</li>
-            </ul>
-        </div>
+
+
+        <footer className="footer-container primary-header primary-navigation">
+            <div className="footer-container--top">
+                <form className="footer-container--top-form">
+                    <input type="text" className="footer-container--input" placeholder="Your email" aria-label="Your email" />
+                    <div>
+                        <button className="btn ff-sans-cond uppercase text-white letter-spacing-2" type="button">Sign up</button>
+                    </div>
+                </form>
+                <div className="footer-container--top-contact">
+                    <button className="btn ff-sans-cond uppercase letter-spacing-2"><Link to="/contact" style={{ color: 'white' }}>Contact Us</Link></button>
+                </div>
+                <div className="footer-container--top-social">
+                    <FaGithub size={30} className="github-icon" />
+                </div>
+            </div>
+            <div className="footer-container--bottom">
+                <div className="text-accent ff-sans-cond letter-spacing-3 text-white">
+                    WelcomeToSpace&copy;{new Date().getFullYear()}
+                </div>
+            </div>
+        </footer>
     )
 }
 
 export default Footer;
+
+
