c084d81c021269006405a3f3ee362b1de6f1bab8
vitaliiptp <vitalii.potapenko@gmail.com>
2021-11-22 17:03:07 +0100

    updated proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-//
-// module.exports = app => {
-//     app.use(createProxyMiddleware("/api/*", { target: "https://space-project-back.herokuapp.com/" }));
-//     // app.use(createProxyMiddleware("/api/*", { target: "http://localhost:3000/" }));
-//
-// };
-//
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = app => {
+    // app.use(createProxyMiddleware("/api/*", { target: "https://space-project-back.herokuapp.com/" }));
+    // app.use(createProxyMiddleware("/api/*", { target: "http://localhost:3000/" }));
+
+};
+
